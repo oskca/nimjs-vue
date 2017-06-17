0b53f36cc1334e4cc42bec8df74f0fd439e33b96
dataright <datago@yeah.net>
2017-06-17 19:57:59 +0800

    using jsObject

--- vue.nim
+++ vue.nim
@@ -1,69 +1,82 @@
-import dom, jsffi, times, strutils, future, macros
+import dom, jsffi, times
 
 type 
-    Option* = ref object
+    Option* = ref object of js 
+        # VueJS instance options
         el* : cstring
-        data* : ref object
+        data* : js
         methods* : js 
-    Vue* {.importc.} = ref object of JsObject
-        data* {.importc:"$$$1".} :JsObject
-        props* {.importc:"$$$1".} :JsObject
+
+type 
+    Instance* {.importc.} = ref object of js
+        data* {.importc:"$$$1".} :js
+        props* {.importc:"$$$1".} :js
         el* {.importc:"$$$1".} : dom.Element
-        options* {.importc:"$$$1".} :JsObject
-        parent* {.importc:"$$$1".} : Vue
-        root* {.importc:"$$$1".} : Vue
-        children* {.importc:"$$$1".} :JsObject
-        slots* {.importc:"$$$1".} :JsObject
-        scopedSlots* {.importc:"$$$1".} :JsObject
-        refs* {.importc:"$$$1".} :JsObject
+        options* {.importc:"$$$1".} :js
+        parent* {.importc:"$$$1".} : Instance
+        root* {.importc:"$$$1".} : Instance
+        children* {.importc:"$$$1".} :js
+        slots* {.importc:"$$$1".} :js
+        scopedSlots* {.importc:"$$$1".} :js
+        refs* {.importc:"$$$1".} :js
         isServer* {.importc:"$$$1".} : bool
 
-        set* {.importc:"$$$1".} : proc(target:JsObject, key:cstring, val: JsObject) 
-        delete* {.importc:"$$$1".} : proc(target:JsObject, key:cstring) 
+        set* {.importc:"$$$1".} : proc(target:js, key:cstring, val: js) 
+        delete* {.importc:"$$$1".} : proc(target:js, key:cstring) 
         watch* {.importc:"$$$1".} : proc(exp:cstring) {.varargs.}
         # event
         on* {.importc:"$$$1".} : proc(event:cstring) {.varargs.}
         emit* {.importc:"$$$1".} : proc(event:cstring) {.varargs.}
 
-var
-    slient* {.importc: "Vue.config.$1".} :bool
-    optionMergeStrategies* {.importc: "Vue.config.$1".} : JsObject
-    devtools* {.importc: "Vue.config.$1".} :bool
-    errorHandler* {.importc: "Vue.config.$1".} :proc(err, vm, info:JsObject)
-    ignoredElements* {.importc: "Vue.config.$1".} :seq[string]
-    keyCodes* {.importc: "Vue.config.$1".} : JsObject
-    performance* {.importc: "Vue.config.$1".} :bool
-    productionTip* {.importc: "Vue.config.$1".} :bool
-    Version* {.importc:"Vue.version".}: cstring
+type
+    configObj* = ref object of js
+        slient*: bool
+        optionMergeStrategies*: js
+        devtools*: bool
+        errorHandler*: proc(err, vm, info:js)
+        ignoredElements*: seq[string]
+        keyCodes*: js
+        performance*: bool
+        productionTip*: bool
 
-proc newVue*(o: Option): Vue {.importcpp:"new Vue(#)"}
+var
+    config* {.importc:"Vue.$1".} : configObj
+    version* {.importc:"Vue.version".}: cstring
+
+proc newVue*(o: js|Option): Instance {.importcpp:"new Vue(#)"}
 
 {.push importc:"Vue.$1".}
-proc extend*(o: Option): Vue
+proc extend*(o: js|Option): Instance
 proc nextTick*(fn:any): void 
-proc set*(target: any, key:cstring|SomeInteger, value: any): void  
-proc delete*(target: any, key:cstring|SomeInteger): void  
-proc directive*(name: cstring, definition: JsObject|proc()): void  
+proc set*(target: any, key:cstring, value: any): void  
+proc delete*(target: any, key:cstring): void
+proc directive*(name: cstring): void {.varargs.}
 proc filter*(name: cstring) {.varargs.}
-proc component*(name: cstring, definition: JsObject): void 
-proc use*(plugin: JsObject): void  
-proc mix*(mix: JsObject): void  
+proc component*(name: cstring): void {.varargs.}
+proc use*(plugin: js): void  
 proc compile*(tmpl: cstring): void
 {.pop.}
 
+proc mix*(mix: js): void {.importc:"Vue.mixin(@)".}
 
 when isMainModule:
-    import jsconsole
+    import future, strutils
+    proc log() {.importcpp:"console.log(@)", varargs.}
     type
-        AO = ref object
+        AO = ref object of js
             str: cstring
             i:int
-    console.log(Version)
-    filter("upper2", (x:cstring)=> cstring(toUpperAscii($x)))
+            list: seq[int]
+    # vue.filter("upper2", (x:cstring)=> cstring(toUpperAscii($x)))
     # var data = AO{
     var model = AO{
             str:"time: $# $#"%[getDateStr(), getClockStr()],
-            i:100,
+            i: 100,
+            f: 120.879,
+            # list: [1,2,3,4],
+            list: lc[x*y | (x <- countup(0,10), y<-1..<10), int],
+            width: dom.window.screen.width,
+            height: dom.window.screen.height,
         }
     # var b = jsnew(Vue)
     var v = newVue(Option(
@@ -71,24 +84,30 @@ when isMainModule:
         data: model,
         methods: js{
             click: proc() = 
-                console.log("111")
+                log("111")
                 model.str = "==============="
-                console.log("222")
+                log("222")
         }
     ))
     # v.set(v, "a", 1.toJs)
-    v.watch("str", (n:cstring) => console.log(n))
-    console.log(v)
-    console.log(v.el)
-    console.log(model)
+    v.watch("str", (n:cstring) => (log(n)))
+    log(v)
+    log(model)
+    # log model.i, model.str, model.f
 
-    slient = false
-    discard window.setInterval(proc() = 
-        model.str = "time: $# $#"%[getDateStr(), getClockStr()],
-    1000)
+    vue.config.slient = true
+    # discard window.setInterval(proc() = 
+    #     model.str = "time: $# $#"%[getDateStr(), getClockStr()],
+    # 1000)
 
-    var a = extend(Option(el:"#id"))
-    console.log(a)
+    # var a = extend(Option{dataProc:()=>js{a:110}})
+    # log(a)
     nextTick(()=>log("nextTick123"))
-    set(v, cstring("b"), 456)
-    console.log("xxxx", "zzzzzzz")
+    # set(v, "b", 456)
+    # delete(v, "b", 456)
+    log("xxxx", "zzzzzzz")
+    config.slient = true    
+    log(version)
+    window.status = "asdf"
+    log(window.status)
+    log(dom.navigator.appCodeName, navigator.appName, navigator.userAgent)
---
+++ vue.nimble
@@ -0,0 +1,6 @@
+# Package
+
+version       = "0.1.0"
+author        = "oskca"
+description   = "VueJS bindings for Nim"
+license       = "MIT"
